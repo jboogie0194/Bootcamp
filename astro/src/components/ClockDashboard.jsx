87ee10723ed2a7521ee7c4d5044dff27e739fabe
jboogie <jeff_eaker@yahoo.com>
2025-03-23 13:24:33 -0500

    digital clock

---
+++ astro/src/components/ClockDashboard.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ClockDashboard() {
+  return (
+    <div>ClockDashboard</div>
+  )
+}
---
+++ astro/src/components/DigitalClock.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+
+export default function DigitalClock() {
+  return (<div> className="DigitalClock"</div>
+  )
+}
