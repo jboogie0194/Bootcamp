b1c83e29b2e5de0fad5c242c40bd0a89c8c3b260
jboogie <jeff_eaker@yahoo.com>
2025-03-25 17:57:20 -0500

    copy of counter, digital clock

--- astro/src/components/DigitalClock.jsx
+++ astro/src/components/DigitalClock.jsx
@@ -1,6 +1,30 @@
-import React from 'react'
+import { useEffect, useState } from "react";
 
-export default function DigitalClock() {
-  return (<div> className="DigitalClock"</div>
-  )
-}
+// Functional component definition
+const DigitalClock: React.FC = () => {
+    const [time, setTime] = useState(new Date());
+
+    // ON COMPONENT LOAD
+    useEffect(() => {
+        const timerId = setInterval(() => {
+            setTime(new Date());
+        }, 1000);
+
+        return () => clearInterval(timerId);
+    }, []);
+
+    return (
+        <div style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+            height: "100vh",
+            fontSize: "48px",
+        }}>
+            <h1>{time.toLocaleString()}</h1>
+        </div>
+    );
+};
+
+export default DigitalClock;
